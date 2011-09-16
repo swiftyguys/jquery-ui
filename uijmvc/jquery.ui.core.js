be97278b02c7a4a145ec90c4d300dcda8a6f2657
robert <robert@heessels.com>
2011-09-16 18:57:01 +0200

    Adapted to new steal.

--- uijmvc/jquery.ui.core.js
+++ uijmvc/jquery.ui.core.js
@@ -1,10 +1,10 @@
 steal(
-	'//jqueryui/ui/jquery.ui.core.js'
-)
-.css(
-	//'../../jqueryui/themes/base/jquery.ui.all'
-	//'../../jqueryui/themes/base/jquery.ui.base',
-	'../../jqueryui/themes/base/jquery.ui.core',
-	'../../jqueryui/themes/base/jquery.ui.theme',
-	'../../jqueryui/uijmvc/fix_size'
+	'//jqueryui/ui/jquery.ui.core.js',
+
+//css
+	//'../../jqueryui/themes/base/jquery.ui.all.css'
+	//'../../jqueryui/themes/base/jquery.ui.base.css',
+	'../../jqueryui/themes/base/jquery.ui.core.css',
+	'../../jqueryui/themes/base/jquery.ui.theme.css',
+	'../../jqueryui/uijmvc/fix_size.css'
 );
--- uijmvc/jquery.ui.dialog.js
+++ uijmvc/jquery.ui.dialog.js
@@ -1,16 +1,18 @@
 steal(
-	'//jqueryui/uijmvc/jquery.ui.core.js',
-	'//jqueryui/uijmvc/jquery.ui.widget.js',
+	'//jqueryui/uijmvc/jquery.ui.core.js'
+).then(
+	'//jqueryui/uijmvc/jquery.ui.widget.js'
+).then(
 	'//jqueryui/uijmvc/jquery.ui.button.js',
 	'//jqueryui/uijmvc/jquery.ui.draggable.js',
 	'//jqueryui/uijmvc/jquery.ui.mouse.js',
 	'//jqueryui/uijmvc/jquery.ui.position.js',
 	'//jqueryui/uijmvc/jquery.ui.resizable.js',
 
-	'//jqueryui/ui/jquery.ui.dialog.js'
-)
-.css(
-	'../../jqueryui/themes/base/jquery.ui.button',
-	'../../jqueryui/themes/base/jquery.ui.resizable',
-	'../../jqueryui/themes/base/jquery.ui.dialog'
+	'//jqueryui/ui/jquery.ui.dialog.js',
+
+//css
+	'../../jqueryui/themes/base/jquery.ui.button.css',
+	'../../jqueryui/themes/base/jquery.ui.resizable.css',
+	'../../jqueryui/themes/base/jquery.ui.dialog.css'
 );
