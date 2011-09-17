d176cb5a5d95f0af693cdff769524e7a860ecbd1
robert <robert@heessels.com>
2011-09-17 18:08:38 +0200

    New steal working. Build working. Yepnope not working in production mode. In develop mode a few refreshes might be needed before number button clicked; probably a load order problem.

--- uijmvc/jquery.ui.dialog.js
+++ uijmvc/jquery.ui.dialog.js
@@ -4,11 +4,12 @@ steal(
 	'//jqueryui/uijmvc/jquery.ui.widget.js'
 ).then(
 	'//jqueryui/uijmvc/jquery.ui.button.js',
-	'//jqueryui/uijmvc/jquery.ui.draggable.js',
 	'//jqueryui/uijmvc/jquery.ui.mouse.js',
-	'//jqueryui/uijmvc/jquery.ui.position.js',
+	'//jqueryui/uijmvc/jquery.ui.position.js'
+).then(
 	'//jqueryui/uijmvc/jquery.ui.resizable.js',
-
+	'//jqueryui/uijmvc/jquery.ui.draggable.js' // order (and then's) important; otherwise buildjs builds faulty production.js
+).then(
 	'//jqueryui/ui/jquery.ui.dialog.js',
 
 //css
