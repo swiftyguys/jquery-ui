0d3fe03aaa2535856cd032a5b901d0bcb8d28132
robert <robert@heessels.com>
2011-09-21 18:03:23 +0200

    Fixed load order.

--- uijmvc/jquery.ui.accordion.js
+++ uijmvc/jquery.ui.accordion.js
@@ -1,6 +1,6 @@
 steal(
 	'//jqueryui/uijmvc/jquery.ui.core.js',
-	'//jqueryui/uijmvc/jquery.ui.widget.js',
-
+	'//jqueryui/uijmvc/jquery.ui.widget.js'
+).then(
 	'//jqueryui/ui/jquery.ui.accordion.js'
 );
